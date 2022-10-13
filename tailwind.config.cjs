ef8fe7e2abc7700c9f072eee2c6d591f37b236ca
Martha Scales <mart1c05@stud.kea.dk>
2022-10-13 10:15:59 +0200

    first

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,48 @@
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      hvid: "#FFFFFF",
+      sort: "#000000",
+      beige: "#EBE1CE",
+      orange: "#FF7556",
+      lysegron: "#C8DEA4",
+      morkegron: "#92A377",
+      graagron: "#D3D9C8",
+      baggrund: "FAF9F8",
+    },
+    fontSize: {
+      h1_mobil: "4rem",
+      h2_mobil: "3rem",
+      h3_mobil: "2rem",
+      h4_mobil: "1.5rem",
+      h5_mobil: "1.2rem",
+      brodtekst_mobil: "1rem",
+      footer_mobil: "0.8rem",
+      footer_mobil_links: "0.8rem",
+      h1_web: "6.25rem",
+      h2_web: "4.375rem",
+      h3_web: "3.125rem",
+      h4_web: "2.8rem",
+      h5_web: "1.5rem",
+      brodtekst_web: "1.3rem",
+      hero_h1: "7rem",
+    },
+
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("daisyui"),
+  ],
+  daisyui: {
+    styled: false,
+    
+  },
 };
+
+
--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,16 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        teamet: resolve(__dirname, "src/teamet_bag.html"),
+        program: resolve(__dirname, "src/program.html"),
+        arkiv: resolve(__dirname, "src/arkiv.html"),
+        arrangoer: resolve(__dirname, "src/arrangoer.html"),
+        billet: resolve(__dirname, "src/billet.html"),
+        frivillig: resolve(__dirname, "src/frivillig.html"),
+        kontakt: resolve(__dirname, "src/kontakt.html"),
+        line_up: resolve(__dirname, "src/line_up.html"),
+        om_vildskud: resolve(__dirname, "src/om_vildskud.html"),
+        sitemap: resolve(__dirname, "src/sitemap.html"),
         // about: resolve(__dirname, "src/about.html"),
       },
     },
